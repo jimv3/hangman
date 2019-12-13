a587a9c0d076fb205e7ba8f568b7eb1dd8a26b4e
Vaughan, Jim <Jim.Vaughan@ingramcontent.com>
2019-12-13 15:57:41 -0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Key extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            letter: props.letter,
+            clicked: false,
+        }
+    }
+    render() {
+        return (
+            <p className="button">{this.state.letter}</p>
+        );
+    }
+}
+class Board extends React.Component {
+    render() {
+        return (
+            <div className="board">
+                <p className="letter">b</p> <p className="letter">l</p> <p className="letter">_</p> <p className="letter">c</p> <p className="letter">k</p>
+            </div>
+        );
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Keyboard extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="keyboard-row">
+                    {'q w e r t y u i o p'.split(' ').map(letter => <Key letter={letter.toUpperCase()} />)}
+                </div>
+                <div className="keyboard-row">
+                    {'a s d f g h j k l'.split(' ').map(letter => <Key letter={letter.toUpperCase()} />)}
+                </div>
+                <div className="keyboard-row">
+                    {'z x c v b n m'.split(' ').map(letter => <Key letter={letter.toUpperCase()} />)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div>
+                <Board />
+                <Keyboard />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
