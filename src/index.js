b67e74917d7c2872ad514b8d8f532faeb66a64ac
Jim Vaughan <jvaughan3@gmail.com>
2019-12-14 08:29:33 -0600

    Begin adding words

--- src/index.js
+++ src/index.js
@@ -2,24 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Key extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            letter: props.letter,
-            clicked: false,
-        }
+class RestartButton extends React.Component {
+    restart(e) {
+        e.preventDefault();
+        window.location.href = 'index.html';
     }
+
     render() {
         return (
-            <p className="button">{this.state.letter}</p>
+            <button type="button" onClick={this.restart.bind(this)}>Restart</button>
+        );
+    }
+}
+class Key extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            letter: props.letter,
+            handleClick: props.clicked,
+        };
+    }
+
+    render() {
+        return (
+            <p className="button" onClick={this.state.handleClick.bind(this)}>{this.state.letter}</p>
         );
     }
 }
 
 class Gallows extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             hangmanPics: [' +---+\n     |\n     |\n     |\n   ===',
                 ' +---+\n O   |\n     |\n     |\n   ===',
@@ -30,45 +43,66 @@ class Gallows extends React.Component {
                 ' +---+\n O   |\n/|\\  |\n/ \\  |\n   ===',
                 ' +---+\n[O   |\n/|\\  |\n/ \\  |\n   ===',
                 ' +---+\n[O]  |\n/|\\  |\n/ \\  |\n   ==='],
-        }
+        };
     }
+    
     render() {
         return (
             <div className="gallows">
                 <pre>
-                    {this.state.hangmanPics[8]}
+                    {this.state.hangmanPics[0]}
                 </pre>
             </div>
-        )
+        );
     }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            letters: props.letters,
+        };
+    }
+
     render() {
         return (
             <div className="board">
-                <p className="letter">b</p> <p className="letter">l</p> <p className="letter">_</p> <p className="letter">c</p> <p className="letter">k</p>
+                {this.state.letters.map(l => <p className="letter">{l}</p>)}
             </div>
         );
     }
 }
 
 class Keyboard extends React.Component {
+    keyboardClick(e) {
+        alert(e.state.letter);
+    }
+
     render() {
         return (
             <div>
                 <div className="keyboard-row">
-                    {'q w e r t y u i o p'.split(' ').map(letter => <Key letter={letter.toUpperCase()} />)}
+                    {'q w e r t y u i o p'.split(' ').map(letter => <Key letter={letter.toUpperCase()} clicked={this.keyboardClick} />)}
                 </div>
                 <div className="keyboard-row">
-                    {'a s d f g h j k l'.split(' ').map(letter => <Key letter={letter.toUpperCase()} />)}
+                    {'a s d f g h j k l'.split(' ').map(letter => <Key letter={letter.toUpperCase()} clicked={this.keyboardClick} />)}
                 </div>
                 <div className="keyboard-row">
-                    {'z x c v b n m'.split(' ').map(letter => <Key letter={letter.toUpperCase()} />)}
+                    {'z x c v b n m'.split(' ').map(letter => <Key letter={letter.toUpperCase()} clicked={this.keyboardClick} />)}
                 </div>
             </div>
         );
     }
+}
+
+function getWord() {
+    let letters = [];
+    for (let i = 0; i < Math.floor(Math.random() * 11) + 3; i++) {
+        letters.push('_');
+    }
+
+    return letters;
 }
 
 class Game extends React.Component {
@@ -76,8 +110,9 @@ class Game extends React.Component {
         return (
             <div>
                 <Gallows />
-                <Board />
+                <Board letters={getWord()} />
                 <Keyboard />
+                <RestartButton />
             </div>
         );
     }
